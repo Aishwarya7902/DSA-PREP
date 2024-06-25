986a2907d5f6a43285e6cb9037d4f0f0faaf917d
AISHWARYA GUPTA <71867316+Aishwarya7902@users.noreply.github.com>
2024-06-25 01:43:06 -0700

    Update N-Queens .cpp

--- RECURSION/N-Queens .cpp
+++ RECURSION/N-Queens .cpp
@@ -53,5 +53,55 @@ public:
 };
 
 /*approach 2
+TC :
+SC:
   */
 
+
+class Solution {
+public:
+    unordered_set<int>cols;
+    unordered_set<int>diag;
+    unordered_set<int>antiDiag;
+    
+    void solve(vector<string>&board,int row,vector<vector<string>>&result){
+        int n=board.size();
+        if(row>=n){
+            result.push_back(board);
+            return;
+        }
+        for(int col=0;col<n;col++){
+           int diagConst=row+col;
+           int antiDiagConst=row-col;
+           if(cols.find(col)!=cols.end() || diag.find(diagConst)!=diag.end() || antiDiag.find(antiDiagConst)!=antiDiag.end()){
+            continue;
+           }
+           //locking col,diag,antiDiag 
+           cols.insert(col);
+           diag.insert(diagConst);
+           antiDiag.insert(antiDiagConst);
+           board[row][col]='Q';
+
+           //calling recursion
+
+           solve(board,row+1,result);
+
+           //backtracking
+
+           cols.erase(col);
+           diag.erase(diagConst);
+           antiDiag.erase(antiDiagConst);
+           board[row][col]='.';
+
+
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>>result;
+        vector<string>board(n,string(n,'.'));
+        
+
+        solve(board,0,result);
+        return result;
+    }
+};
