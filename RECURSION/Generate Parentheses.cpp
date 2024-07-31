80e13dbbca1084d3663109e5c956597232678b59
AISHWARYA GUPTA <71867316+Aishwarya7902@users.noreply.github.com>
2024-07-31 08:32:13 -0700

    Update Generate Parentheses.cpp

--- RECURSION/Generate Parentheses.cpp
+++ RECURSION/Generate Parentheses.cpp
@@ -3,7 +3,7 @@ video:https://www.youtube.com/watch?v=7xkPbffc6w8&list=PLpIkg8OmuX-IBcXsfITH5ql0
 
 /*
   APPROACH 1
-  TC:O(2^2n )
+  TC:O(2^2n *n)
   */
 class Solution {
 public:
@@ -37,6 +37,45 @@ public:
         vector<string>result;
         string curr="";
         solve(curr,n,result);
+        return result;
+    }
+};
+
+/*
+  APPROACH 2
+  TC:O(2^2n ) since there are n pairs(pair means 2*n) and at every place , we have 2 choices ,hence tc will be O(2^2n )
+  */
+
+class Solution {
+public:
+    
+    void solve(string &curr,int n,int open,int close,vector<string>&result){
+        //base case
+        if(curr.size()==2*n){
+            result.push_back(curr);
+            return;
+        }
+
+        if(open<n){
+          curr.push_back('(');//try
+          solve(curr,n,open+1,close,result);//explore
+        //backtrack
+          curr.pop_back();
+        }
+        if(close<open){
+            curr.push_back(')'); //try new possibility
+            solve(curr,n,open,close+1,result);//explore
+            curr.pop_back();
+        }
+        
+
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string>result;
+        string curr="";
+        int open=0;
+        int close=0;
+        solve(curr,n,open,close,result);
         return result;
     }
 };
