cdbdd49cdfe1f50d9d122e54f865636d24928f25
AISHWARYA GUPTA <71867316+Aishwarya7902@users.noreply.github.com>
2024-08-06 05:41:26 -0700

    Update Permutations II.cpp

--- RECURSION/Permutations II.cpp
+++ RECURSION/Permutations II.cpp
@@ -1,9 +1,9 @@
 problem: https://leetcode.com/problems/permutations-ii/description/
-VIDEO :https://www.youtube.com/watch?v=Vd2YizhbN74&list=PLpIkg8OmuX-IBcXsfITH5ql0Lqci1MYPM&index=16
+
 /*
   APPROACH 1
   TC:O(N∗N!)
-  SC:
+  SC:O(N)+O(N) //taking map and temp
 */
 
 
@@ -37,6 +37,36 @@ public:
         }
 
         solve(temp,mp,result,n);
+        return result;
+    }
+};
+
+/*
+APPROACH 2
+  TC: O(N*N!) //n! coz of permutation and n coz we have to push temp in result which is a copy operation
+  SC:O(N)
+*/
+
+class Solution {
+public:
+    void solve(int indx,vector<int>& nums,vector<vector<int>>&result){
+        if(indx>=nums.size()){
+            result.push_back(nums);
+            return;
+        }
+        unordered_set<int>st;
+        for(int i=indx;i<nums.size();i++){
+            if(st.find(nums[i])==st.end()){
+                st.insert(nums[i]);
+                swap(nums[i],nums[indx]); //do
+                solve(indx+1,nums,result); //explore
+                swap(nums[i],nums[indx]); // backtrack
+            }
+        }
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>>result;
+        solve(0,nums,result);
         return result;
     }
 };
