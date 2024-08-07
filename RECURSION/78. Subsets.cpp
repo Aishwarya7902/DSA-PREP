8d04a76c9d432e1c7fbf43e0704937c1ff4832e6
AISHWARYA GUPTA <71867316+Aishwarya7902@users.noreply.github.com>
2024-08-07 09:59:28 -0700

    Create 78. Subsets.cpp

---
+++ RECURSION/78. Subsets.cpp
@@ -0,0 +1,30 @@
+PROBLEM:https://leetcode.com/problems/subsets/description/
+
+/*
+  APPROACH 1
+  TC: O(2^N) // at every place we have two choices
+  SC:
+  */
+
+class Solution {
+public:
+    void solve(int indx,vector<int>& nums,vector<int>&curr,vector<vector<int>>&result){
+        //base case
+        if(indx>=nums.size()){
+            result.push_back(curr);
+            return;
+        }
+
+        curr.push_back(nums[indx]); //do
+        solve(indx+1,nums,curr,result); //explore
+        curr.pop_back(); //backtrack
+         solve(indx+1,nums,curr,result);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>>result;
+        vector<int>curr;
+
+        solve(0,nums,curr,result);
+        return result;
+    }
+};
