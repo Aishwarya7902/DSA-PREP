dffa304723b932e70ef63aa8700a41ee52f1ca70
AISHWARYA GUPTA <71867316+Aishwarya7902@users.noreply.github.com>
2024-09-11 12:53:56 -0700

    Update 389. Find the Difference.cpp

--- LeetCode_Easy/389. Find the Difference.cpp
+++ LeetCode_Easy/389. Find the Difference.cpp
@@ -1,1 +1,40 @@
+//approach 1
 
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        unordered_map<char,int>mp;
+        
+        for(char ch:s){
+            mp[ch]++;
+        }
+
+        for(char ch:t){
+            mp[ch]--;
+            if(mp[ch]<0)return ch;
+        }
+
+       
+     return 'x';
+    }
+};
+
+//approach 2
+
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        unordered_map<char,int>mp;
+        int xorr=0;
+        for(char &ch:s){
+           xorr^=ch;
+        }
+
+        for(char ch:t){
+            xorr^=ch;
+        }
+
+       
+     return (char)xorr;
+    }
+};
