64d6ede864d2c7ac3066b334a72da47662892f2a
AISHWARYA GUPTA <71867316+Aishwarya7902@users.noreply.github.com>
2024-10-27 05:32:43 -0700

    Update 146. LRU Cache.cpp
    OPTIMISATION

--- DESIGN/146. LRU Cache.cpp
+++ DESIGN/146. LRU Cache.cpp
@@ -45,5 +45,58 @@ public:
     }
 };
 
+/*
+OPTIMISATION
+TC:O(1) for get and put operations
+*/
 
+class LRUCache {
+public:
+    list<int>dll;
+    map<int,pair<list<int>::iterator,int>>mp;
+    int n;
+
+    void makeRecentlyUsedKey(int key){
+        dll.erase(mp[key].first);
+        dll.push_front(key);
+        mp[key].first=dll.begin();
+    }
+    LRUCache(int capacity) {
+        n=capacity;
+    }
+    
+    int get(int key) {
+        if(mp.find(key)==mp.end())return -1;
+
+        makeRecentlyUsedKey(key);
+        return mp[key].second;
+    }
+    
+    void put(int key, int value) {
+        if(mp.find(key)!=mp.end()){
+            makeRecentlyUsedKey(key);
+            mp[key].second=value;
+        }
+
+        else{
+            dll.push_front(key);
+            mp[key]={dll.begin(),value};
+            n--;
+        }
+
+        if(n<0){
+            int LRU_key=dll.back();
+            mp.erase(LRU_key);
+            dll.pop_back();
+            n++;
+        }
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
   
