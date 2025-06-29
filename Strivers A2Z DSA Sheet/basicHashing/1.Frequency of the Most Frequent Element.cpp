9062d4f319df1ba64bc383651e4a963b2c87f9b2
AISHWARYA GUPTA <71867316+Aishwarya7902@users.noreply.github.com>
2025-06-29 01:55:23 -0700

    Update 1.Frequency of the Most Frequent Element.cpp

--- Strivers A2Z DSA Sheet/basicHashing/1.Frequency of the Most Frequent Element.cpp
+++ Strivers A2Z DSA Sheet/basicHashing/1.Frequency of the Most Frequent Element.cpp
@@ -28,3 +28,56 @@ public:
         return max_freq;
     }
 };
+
+/*
+OPTIMIZATION
+USING BINARY SEARCH
+
+Tc : O(nlogn) + O(n * logn)
+SC: O(N)
+
+*/
+
+class Solution {
+public:
+    int helperBsearch(int target,vector<int>& nums, int k,vector<long>&prefix){
+        int l=0,r=target,bestMid=target;
+        while(l<=r){
+            int mid =  l +(r-l)/2;
+            long count= target-mid+1;
+            long windowSum=count*nums[target];
+            long actualSum=prefix[target]-prefix[mid]+nums[mid];
+
+            int ops=windowSum-actualSum;
+            if(ops>k){
+                l=mid+1;
+            }
+            else{
+                   bestMid=mid;
+                   r=mid-1;
+            }
+
+
+
+        }
+        return target-bestMid+1;
+    }
+    int maxFrequency(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end()); // TC :nlogn
+        vector<long> prefix(nums.size());
+        prefix[0]=nums[0];
+        for(int i=1;i<nums.size();i++){ // TC :o(n)
+            
+            prefix[i]=prefix[i-1]+nums[i];
+        }
+        
+
+        int result=0;
+        for(int i=0;i<nums.size();i++){ // TC :(n)
+            int freq=helperBsearch(i,nums,k,prefix); // TC :logn
+            result=max(result,freq);
+        }
+
+        return result;
+    }
+};
