a0db354955d2fe3b19248c3b3e069c87d6968ba0
AISHWARYA GUPTA <71867316+Aishwarya7902@users.noreply.github.com>
2025-07-13 12:03:44 +0530

    Update 1.Frequency of the Most Frequent Element.cpp
    improved solution using sliding window

--- Strivers A2Z DSA Sheet/basicHashing/1.Frequency of the Most Frequent Element.cpp
+++ Strivers A2Z DSA Sheet/basicHashing/1.Frequency of the Most Frequent Element.cpp
@@ -81,3 +81,34 @@ public:
         return result;
     }
 };
+
+/*
+  using sliding window
+//T.C : O(nlogn)
+//S.C : O(1)
+*/
+
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        int result=0;
+        long currSum=0;
+
+        int l=0;
+        for(int r=0;r<nums.size();r++){
+            long target=nums[r];
+            currSum+= nums[r];
+            long windowSum= (r-l+1)*target;
+            long ops= windowSum-currSum;
+
+            if(ops>k){
+                currSum-=nums[l];
+                l++;
+            }
+
+            result=max(result,r-l+1);
+        }
+        return result;
+    }
+};
