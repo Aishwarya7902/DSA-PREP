b95a01d95ab63fcd549249a3155c1c5912366640
AISHWARYA GUPTA <71867316+Aishwarya7902@users.noreply.github.com>
2025-07-29 15:03:54 +0530

    Update 2. Check if Array Is Sorted and Rotated.cpp

--- Strivers A2Z DSA Sheet/Problems on Arrays/easy/2. Check if Array Is Sorted and Rotated.cpp
+++ Strivers A2Z DSA Sheet/Problems on Arrays/easy/2. Check if Array Is Sorted and Rotated.cpp
@@ -4,7 +4,7 @@
 BRUTE FORCE
 make all the possible rotations , i.e rotate the array by r=0,1,2,3....till n and at any point array is sorted return true...else false
 
-tc:o(n^2)
+tc:o(n*(n+n))  = O(n*2n)
 sc:o(n)
 */
 
@@ -34,5 +34,52 @@ public:
         }
 
         return false;
+    }
+};
+
+/*
+OPTIMIZATION
+TC: O(n*n)
+SC: O(1)
+*/
+
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>sorted=nums;
+        sort(sorted.begin(),sorted.end());
+
+        for(int r=0;r<n;r++){
+            bool isSorted=true;
+            for(int i=0;i<n;i++){
+                if(sorted[i]!=nums[(i+r)%n]){
+                   isSorted=false;
+                }
+            }
+
+            if(isSorted)return true;
+        }
+        return false;
+    }
+};
+
+/*
+BEST APPROACH
+TC:O(N)
+SC:O(1)
+*/
+
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int n=nums.size();
+        int peak=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]>nums[(i+1)%n])peak++;
+           
+        }
+
+        return peak>1 ? false :true;
     }
 };
