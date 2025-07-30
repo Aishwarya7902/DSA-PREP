34fd798842e1a2c2f0a8ed5f2d98711d7fa188df
AISHWARYA GUPTA <71867316+Aishwarya7902@users.noreply.github.com>
2025-07-30 15:09:16 +0530

    Update 3.Remove Duplicates from Sorted Array.cpp

--- Strivers A2Z DSA Sheet/Problems on Arrays/easy/3.Remove Duplicates from Sorted Array.cpp
+++ Strivers A2Z DSA Sheet/Problems on Arrays/easy/3.Remove Duplicates from Sorted Array.cpp
@@ -28,3 +28,29 @@ public:
         return k;
     }
 };
+
+/*
+OPTIMIZATION
+USING 2 POINTER
+TC:O(N)
+SC:O(1)
+*/
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1)return 1;
+
+        int i=0;
+        for(int j=1;j<n;j++){
+            if(nums[j]!=nums[i]){
+                nums[i+1]=nums[j];
+                i++;
+                
+            }
+        }
+        return i+1;
+
+    }
+};
