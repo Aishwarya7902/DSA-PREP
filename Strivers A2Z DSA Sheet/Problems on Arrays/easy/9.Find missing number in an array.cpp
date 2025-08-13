fd28d06a44eadc537e65edcb43f032c9c06d948b
AISHWARYA GUPTA <71867316+Aishwarya7902@users.noreply.github.com>
2025-08-13 21:47:01 +0530

    Update 9.Find missing number in an array.cpp

--- Strivers A2Z DSA Sheet/Problems on Arrays/easy/9.Find missing number in an array.cpp
+++ Strivers A2Z DSA Sheet/Problems on Arrays/easy/9.Find missing number in an array.cpp
@@ -41,3 +41,82 @@ public:
         return 0;
     }
 };
+
+
+/*
+
+optimization
+
+tc: 
+average / expected time: Θ(n).
+unordered_map provides amortized O(1) insert/find on average (hash-table).
+
+Worst-case time: O(n²)
+
+Space: Θ(n).
+
+*/
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        int n=nums.size();
+
+        for(auto it:nums){ 
+            mp[it]++;
+        }
+
+        for(int i=0;i<=n;i++){ 
+            if(mp.find(i)==mp.end())return i;
+        }
+
+        return 0;
+    }
+};
+
+/*
+Best Aprroach
+TC:O(N)
+sc:O(1)
+*/
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        
+        int n=nums.size();
+        int sumOfNatural=n*(n+1)/2;
+
+        int sum=0;
+        for(auto it:nums){
+            sum+=it;
+        }
+
+        return sumOfNatural-sum;
+    }
+
+};
+
+/*
+using xor
+tc:o(n)
+sc:o(1)
+*/
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+       int xorr=0;
+
+       for(int i=0;i<=nums.size();i++){
+        xorr=xorr^i;
+       }
+
+       for(int i=0;i<nums.size();i++){
+        xorr=xorr^nums[i];
+       }
+       return xorr;
+    }
+
+};
